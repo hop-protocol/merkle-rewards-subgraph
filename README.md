ed5357928ebbaf9fc0e66fa1493aee832722bef3
Shane Fontaine <9441295+shanefontaine@users.noreply.github.com>
2022-06-21 13:22:22 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Merkle Rewards Subgraph
+
+The Subgraph for MerkleRewards
+
+## Development
+
+Build
+```bash
+npm run build
+```
+
+Generate config
+```bash
+npm run codegen
+```
+
+
+Deploy
+```bash
+npm run deploy
+```
+
+## License
+
+[MIT](LICENSE)
---
+++ src/mapping.ts
@@ -0,0 +1,17 @@
+import { Claimed } from "../generated/MerkleRewards/MerkleRewards"
+import { ClaimedEntity } from "../generated/schema"
+
+export function handleClaimed(event: Claimed): void {
+  const id = event.transaction.hash.toHexString().concat(event.transactionLogIndex.toString())
+  let entity = ClaimedEntity.load(id)
+  if (entity == null) {
+    entity = new ClaimedEntity(id)
+  }
+
+  entity.account = event.params.account
+  entity.amount = event.params.amount
+  entity.totalAmount = event.params.totalAmount
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
